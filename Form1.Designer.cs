c159dcb9aad74a84e68d86705937d75b7572a766
Jack Northover <northovers23@gmail.com>
2018-06-26 16:58:30 +1200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace geuss_my_number
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_geuss = new System.Windows.Forms.Button();
+            this.Lb_output = new System.Windows.Forms.Label();
+            this.tbox_guess = new System.Windows.Forms.TextBox();
+            this.lb_left = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_geuss
+            // 
+            this.btn_geuss.Location = new System.Drawing.Point(204, 283);
+            this.btn_geuss.Name = "btn_geuss";
+            this.btn_geuss.Size = new System.Drawing.Size(160, 80);
+            this.btn_geuss.TabIndex = 0;
+            this.btn_geuss.Text = "guess";
+            this.btn_geuss.UseVisualStyleBackColor = true;
+            this.btn_geuss.Click += new System.EventHandler(this.geuss_Click);
+            // 
+            // Lb_output
+            // 
+            this.Lb_output.AutoSize = true;
+            this.Lb_output.Location = new System.Drawing.Point(215, 103);
+            this.Lb_output.Name = "Lb_output";
+            this.Lb_output.Size = new System.Drawing.Size(134, 20);
+            this.Lb_output.TabIndex = 1;
+            this.Lb_output.Text = "guess my number";
+            // 
+            // tbox_guess
+            // 
+            this.tbox_guess.Location = new System.Drawing.Point(151, 215);
+            this.tbox_guess.Name = "tbox_guess";
+            this.tbox_guess.Size = new System.Drawing.Size(261, 26);
+            this.tbox_guess.TabIndex = 2;
+            // 
+            // lb_left
+            // 
+            this.lb_left.AutoSize = true;
+            this.lb_left.Location = new System.Drawing.Point(219, 159);
+            this.lb_left.Name = "lb_left";
+            this.lb_left.Size = new System.Drawing.Size(108, 20);
+            this.lb_left.TabIndex = 3;
+            this.lb_left.Text = "Guess left: 10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 518);
+            this.Controls.Add(this.lb_left);
+            this.Controls.Add(this.tbox_guess);
+            this.Controls.Add(this.Lb_output);
+            this.Controls.Add(this.btn_geuss);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_geuss;
+        private System.Windows.Forms.Label Lb_output;
+        private System.Windows.Forms.TextBox tbox_guess;
+        private System.Windows.Forms.Label lb_left;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace geuss_my_number
+{
+    public partial class Form1 : Form
+    {
+        int number;
+        int guess;
+        int count;
+        Random R_ran = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            number = R_ran.Next(1, 100);
+            count = 10;
+        }
+
+        private void geuss_Click(object sender, EventArgs e)
+        {
+            guess = Convert.ToInt32(tbox_guess.Text);
+            
+            if(guess == number)
+            {
+                Lb_output.Text = "That is correct.";
+            }
+            else if (guess > number)
+            {
+                Lb_output.Text = "That is too high.";
+                count--;
+                lb_left.Text = "Guess left: " + Convert.ToString(count);
+                losecheck();
+            }
+            else if (guess < number)
+            {
+                Lb_output.Text = "That is too low.";
+                count--;
+                lb_left.Text = "Guess left: " + Convert.ToString(count);
+                losecheck();
+            }
+
+
+        }
+        private void losecheck()
+        {
+            if (count == 0)
+            {
+                DialogResult dialogResult = MessageBox.Show("game over, would you like to play again?", "game over", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    number = R_ran.Next(1, 100);
+                    count = 10;
+                    Lb_output.Text = "guess my new number";
+                    lb_left.Text = "Guess left: " + Convert.ToString(count);
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    Application.Exit();
+                }
+            }
+        }
+    }
+}
